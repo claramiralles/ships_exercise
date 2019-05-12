b519db560c5957adfde59e72154351f1ad5b5891
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-05-12 14:03:02 +0200

    publish github pages

---
+++ docs/precache-manifest.f96fa6f35ee6dc9ee88822e683e3255b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3e9551500a4eb93a3f213c156b6ac4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a19e5f450fe3c27909bc",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "./static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "a19e5f450fe3c27909bc",
+    "url": "./static/js/main.e55ab3a9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
